af64b51fa375a8a0fc56cf89be24207041ac2068
MOHAMMADNIBRAZN <85448180+MOHAMMADNIBRAZN@users.noreply.github.com>
2021-06-12 16:33:52 +0530

    Add files via upload

---
+++ ODDOREVEN.java
@@ -0,0 +1,22 @@
+package ODDOREVEN;
+
+public class ODDOREVEN {
+
+	public static void main(String[] args) {
+		
+		int num=11;
+		
+		if (num % 2 == 0)
+			
+			System.out.println("enter number is even");
+		
+		else
+			
+			System.out.println("enter number is odd");
+			
+			
+			
+
+	}
+
+}
